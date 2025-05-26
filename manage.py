8159130a2371ce78e74f7d02bb99d8aff464f0e3
DenisJju <denisjju@gmail.com>
2025-05-26 17:49:21 +0300

    Add files via upload

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardiac_ml_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardiacmlapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
